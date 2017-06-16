ba2955b72ceda38493f401f3b7c5344bb2664b06
Luke Lan <luke84320136@gmail.com>
2017-06-16 21:03:12 +0800

    6/16 Structing character
    1.Character.vue add character-card

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="" @click= "switchCharacter">
-    {{character}}
+  <div class="col-md-4" @click= "switchCharacter">
+    <div class="character-card">
+      <div class="card-block">
+        <h4 class= "card-title">{{character.name}}</h4>
+         <p class= "card-text">身高: {{character.height}}公分</p>
+         <p class= "card-text">體重: {{character.mass}}公斤</p>
+         <p class= "card-text">髮色: {{character.hair_color}}</p>
+         <p clas= "card-text">眼睛的顏色: {{character.eye_color}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
